15fb145a08f77930627009f4689fecb3c7a8efe9
fabiorsantos86 <fabiorsantos86@gmail.com>
2026-01-14 16:00:02 -0300

    Add files via upload

---
+++ Modulo 2 Columns.sql
@@ -0,0 +1,108 @@
+create table this_table_has_three_columns (
+  this_is_a_character_column varchar2(100),
+  this_is_a_number_column    number,
+  this_is_a_date_column      date
+);
+
+select table_name, column_name, data_type, data_length, data_precision, data_scale
+from   user_tab_columns;
+
+create table toys (
+  toy_name varchar2(10)
+);
+
+select column_name, data_type, data_length
+from   user_tab_columns
+where  table_name = 'TOYS';
+
+create table character_data (
+  varchar_10_col   varchar2(10),
+  varchar_4000_col varchar2(4000),
+  char_10_col      char(10),
+  clob_col         clob
+);
+
+select column_name, data_type, data_length
+from   user_tab_columns
+where  table_name = 'CHARACTER_DATA';
+
+create table numeric_data (
+  number_3_sf_2_dp  number(3, 2),
+  number_3_sf_2     number(3, -2),
+  number_5_sf_0_dp  number(5, 0),
+  integer_col       integer,
+  float_col         float(10),
+  real_col          real,
+  binary_float_col  binary_float,
+  binary_double_col binary_double
+);
+
+select column_name, data_type, data_length, data_precision, data_scale
+from   user_tab_columns
+where  table_name = 'NUMERIC_DATA';
+
+create table datetime_data (
+  date_col                      date,
+  timestamp_with_3_frac_sec_col timestamp(3),
+  timestamp_with_tz             timestamp with time zone,
+  timestamp_with_local_tz       timestamp with local time zone,
+  year_to_month_col             interval year to month,
+  day_to_second_col             interval day to second
+);
+
+select column_name, data_type, data_length, data_precision, data_scale
+from   user_tab_columns
+where  table_name = 'DATETIME_DATA';
+
+
+create table binary_data (
+  raw_col  raw(1000),
+  blob_col blob
+);
+
+select column_name, data_type, data_length, data_precision, data_scale
+from   user_tab_columns
+where  table_name = 'BINARY_DATA';
+
+create table bricks2 (
+    brick_id number(20, 0),
+    colour varchar2(10),
+    price number (10,2),
+    purchased_date date
+);
+
+select column_name, data_type, data_length, data_precision, data_scale
+from   user_tab_columns
+where  table_name = 'BRICKS2';
+
+alter table this_table_has_three_columns add (
+  this_is_a_timestamp_column    timestamp,
+  this_is_a_binary_large_object blob
+);
+
+select column_name, data_type, data_length, data_precision, data_scale
+from   user_tab_columns
+where  table_name = 'THIS_TABLE_HAS_THREE_COLUMNS';
+
+alter table this_table_has_three_columns drop (
+  this_is_a_timestamp_column,
+  this_is_a_binary_large_object
+);
+
+select column_name, data_type, data_length, data_precision, data_scale
+from   user_tab_columns
+where  table_name = 'THIS_TABLE_HAS_THREE_COLUMNS';
+
+drop table toys;
+create table toys (
+  toy_id            integer,
+  toy_name          varchar2(100),
+  cuddliness_factor integer
+);
+
+alter table toys add (toy_weight number(8,1));
+alter table toys drop (cuddliness_factor);
+
+select column_name, data_type, data_length, data_precision, data_scale
+from   user_tab_columns
+where  table_name = 'TOYS';
